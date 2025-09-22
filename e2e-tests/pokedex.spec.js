69578f9c6b277f5b7c0b5af908437a8ff48679bb
Robert Pakkanen <rpakkane@gmail.com>
2025-09-22 13:40:46 +0300

    Add pokemon page e2e test

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -6,4 +6,10 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
+
+  test('pokemon page can be navigated to', async ({ page }) => {
+    await page.goto('/pokemon/bulbasaur')
+    await expect(page.getByText('bulbasaur')).toBeVisible()
+    await expect(page.getByText('overgrow')).toBeVisible()
+  })
 })
